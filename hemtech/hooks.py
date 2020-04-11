2bcf42beab0d829d3f3e80deacc19237387d3e1c
Finbyz <info@finbyz.com>
2020-04-11 06:37:27 +0000

    Removed Build

--- hemtech/hooks.py
+++ hemtech/hooks.py
@@ -18,17 +18,6 @@ app_license = "GPL 3.0"
 # app_include_css = "/assets/hemtech/css/hemtech.css"
 # app_include_js = "/assets/hemtech/js/hemtech.js"
 
-app_include_css =  [
-	"/assets/css/hemtech.min.css",
-	"assets/css/summernote.min.css"
-]
-
-app_include_js = [
-	"assets/js/summernote.min.js",
-	"assets/js/comment_desk.min.js",
-	"assets/js/editor.min.js",
-	"assets/js/timeline.min.js"
-]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/hemtech/css/hemtech.css"
